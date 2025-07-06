5ed2f6841aec6d0b11a3501dc22de0f1613a883f
Amaan shamim khan <amaans1912@gmail.com>
2025-07-06 06:45:37 +0530

    Fixing Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Hello, I’m Amaan 👋</h1>
+      <p>This is my Habit Tracker comeback project!</p>
+      <p>I’m rebuilding my routine, one line of code at a time.</p>
+    </div>
+  );
+}
+
+export default App;
